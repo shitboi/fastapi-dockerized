3896c1f9faa56ecac1687f5daf765fb4aa4e4f9a
ayoshittu <55026063+shitboi@users.noreply.github.com>
2025-12-17 09:07:14 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,13 +17,13 @@ def get_servers():
     data = pd.read_excel('servers.xlsx').to_dict()
     r = [[str(s) for s in list(k.values())] for k in list(data.values())]        
     cols = ['id', 'ipAddress', 'country', 'region', 'opscodeBranch', 'description', 
-            'provider', 'puppetLastRun', 'decrpyptkey', 'status']
+            'provider', 'puppetLastRun', 'status']
     
-    g = [(a,b,c,d,e,f,g,h,i,j) for a,b,c,d,e,f,g,h,i,j 
-         in zip(r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9])]
+    g = [(a,b,c,d,e,f,g,h,i) for a,b,c,d,e,f,g,h,i 
+         in zip(r[0],r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8])]
     
     records = [{cols[0]: r[0],cols[1]: r[1],cols[2]: r[2],cols[3]: r[3],cols[4]: r[4],
-        cols[5]: r[5],cols[6]: r[6],cols[7]: r[7],cols[8]: r[8],cols[9]: r[9]}
+        cols[5]: r[5],cols[6]: r[6],cols[7]: r[7],cols[8]: r[8]}
          for r in g]
     return records
 
